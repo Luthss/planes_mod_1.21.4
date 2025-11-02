7750fb9d2dd724fbdb18b81ac01fad4e8d1d1ad8
Luthss <ismailloothmohamed@gmail.com>
2025-11-02 13:01:45 +0500

    Set up done

---
+++ src/main/java/com/luth/planesmod/PlanesClient.java
@@ -0,0 +1,10 @@
+package com.luth.planesmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlanesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
